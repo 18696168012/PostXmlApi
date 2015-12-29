9f44ffbed122983cb28342b59c1780eaff511f1a
gator <791264638@qq.com>
2015-12-29 09:17:00 +0800

    使用证书，以post方式提交xml到对应的接口url

--- PostXmlApi.php
+++ PostXmlApi.php
@@ -37,3 +37,47 @@
 			return false;
 		}
 	}
+
+
+	/**
+	 * 	作用：使用证书，以post方式提交xml到对应的接口url
+	 */
+	function postXmlSSLCurl($xml,$url,$second=30){
+		$ch = curl_init();
+		//超时时间
+		curl_setopt($ch,CURLOPT_TIMEOUT,$second);
+		//这里设置代理，如果有的话
+        //curl_setopt($ch,CURLOPT_PROXY, '8.8.8.8');
+        //curl_setopt($ch,CURLOPT_PROXYPORT, 8080);
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
+		//设置header
+		curl_setopt($ch,CURLOPT_HEADER,FALSE);
+		//要求结果为字符串且输出到屏幕上
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+		//设置证书
+		//使用证书：cert 与 key 分别属于两个.pem文件
+		//默认格式为PEM，可以注释
+		//curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+		//curl_setopt($ch,CURLOPT_SSLCERT, WxPayConf_pub::SSLCERT_PATH);
+		//默认格式为PEM，可以注释
+		//curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+		//curl_setopt($ch,CURLOPT_SSLKEY, WxPayConf_pub::SSLKEY_PATH);
+		//post提交方式
+		curl_setopt($ch,CURLOPT_POST, true);
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$xml);
+		$data = curl_exec($ch);
+		//返回结果
+		if($data){
+			curl_close($ch);
+			return $data;
+		}
+		else{ 
+			$error = curl_errno($ch);
+			echo "curl出错，错误码:$error"."<br>"; 
+			echo "<a href='http://curl.haxx.se/libcurl/c/libcurl-errors.html'>错误原因查询</a></br>";
+			curl_close($ch);
+			return false;
+		}
+	}
